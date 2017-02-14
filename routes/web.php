9dffb8a1020829ad7990d6bd9bdcb435c3e88dde
AlbertoRoman1992 <albertoroman92@hotmail.com>
2017-02-14 21:06:08 +0100

    primer commit

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/proyectos/{id?}', function ($id = null) {
+    if (isset($id)) {
+        $proyecto = App\Proyecto::findOrFail($id);
+
+        return $proyecto;
+//        $proyectos = \Auth::getUser()->proyectos;
+//
+//        foreach ($proyectos as $proyecto)
+//            echo $proyecto->isValidado();
+    }
+    else
+        return App\Proyecto::all();
+});
+
+Route::get('/perfil', function () {
+    $user = Auth::user();
+
+    echo $user->name;
+
+    foreach ($user->proyectos as $proyecto) {
+        echo '<li>' . $proyecto->nombre . '</li>';
+    }
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
